c608a516d5fc7ba893798357c4ac90777283b705
kryzysoft <kryzysoft@gmail.com>
2019-07-06 22:41:59 +0200

    ESP working

--- ArcBase_Prg/src/main.c
+++ ArcBase_Prg/src/main.c
@@ -37,16 +37,22 @@
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
+#include "nrf_esb.h"
+
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
-
+#include "sdk_common.h"
 #include "nrf.h"
+#include "nrf_esb_error_codes.h"
+#include "nrf_delay.h"
 #include "app_util_platform.h"
 #include "nrf_drv_usbd.h"
 #include "nrf_drv_clock.h"
 #include "nrf_gpio.h"
+#include "nrf_error.h"
 #include "nrf_drv_power.h"
+#include "boards.h"
 
 #include "app_timer.h"
 #include "app_usbd.h"
@@ -212,6 +218,103 @@ APP_USBD_HID_GENERIC_GLOBAL_DEF(m_app_hid_generic,
                                 APP_USBD_HID_PROTO_MOUSE);
 
 /*lint -restore*/
+
+
+
+
+
+
+
+uint8_t led_nr;
+
+nrf_esb_payload_t rx_payload;
+
+/*lint -save -esym(40, BUTTON_1) -esym(40, BUTTON_2) -esym(40, BUTTON_3) -esym(40, BUTTON_4) -esym(40, LED_1) -esym(40, LED_2) -esym(40, LED_3) -esym(40, LED_4) */
+
+void nrf_esb_event_handler(nrf_esb_evt_t const * p_event)
+{
+    switch (p_event->evt_id)
+    {
+        case NRF_ESB_EVENT_TX_SUCCESS:
+            NRF_LOG_DEBUG("TX SUCCESS EVENT");
+            break;
+        case NRF_ESB_EVENT_TX_FAILED:
+            NRF_LOG_DEBUG("TX FAILED EVENT");
+            break;
+        case NRF_ESB_EVENT_RX_RECEIVED:
+            NRF_LOG_DEBUG("RX RECEIVED EVENT");
+            if (nrf_esb_read_rx_payload(&rx_payload) == NRF_SUCCESS)
+            {
+                // Set LEDs identical to the ones on the PTX.
+                nrf_gpio_pin_write(LED_1, !(rx_payload.data[1]%8>0 && rx_payload.data[1]%8<=4));
+                nrf_gpio_pin_write(LED_2, !(rx_payload.data[1]%8>1 && rx_payload.data[1]%8<=5));
+                nrf_gpio_pin_write(LED_3, !(rx_payload.data[1]%8>2 && rx_payload.data[1]%8<=6));
+                nrf_gpio_pin_write(LED_4, !(rx_payload.data[1]%8>3));
+
+                NRF_LOG_DEBUG("Receiving packet: %02x", rx_payload.data[1]);
+            }
+            break;
+    }
+}
+
+void clocks_start( void )
+{
+    NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+    NRF_CLOCK->TASKS_HFCLKSTART = 1;
+
+    while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0);
+}
+
+
+void gpio_init( void )
+{
+    bsp_board_init(BSP_INIT_LEDS);
+}
+
+
+uint32_t esb_init( void )
+{
+    uint32_t err_code;
+    uint8_t base_addr_0[4] = {0xE7, 0xE7, 0xE7, 0xE7};
+    uint8_t base_addr_1[4] = {0xC2, 0xC2, 0xC2, 0xC2};
+    uint8_t addr_prefix[8] = {0xE7, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8 };
+    nrf_esb_config_t nrf_esb_config         = NRF_ESB_DEFAULT_CONFIG;
+    nrf_esb_config.payload_length           = 8;
+    nrf_esb_config.protocol                 = NRF_ESB_PROTOCOL_ESB_DPL;
+    nrf_esb_config.bitrate                  = NRF_ESB_BITRATE_2MBPS;
+    nrf_esb_config.mode                     = NRF_ESB_MODE_PRX;
+    nrf_esb_config.event_handler            = nrf_esb_event_handler;
+    nrf_esb_config.selective_auto_ack       = false;
+
+    err_code = nrf_esb_init(&nrf_esb_config);
+    VERIFY_SUCCESS(err_code);
+
+    err_code = nrf_esb_set_base_address_0(base_addr_0);
+    VERIFY_SUCCESS(err_code);
+
+    err_code = nrf_esb_set_base_address_1(base_addr_1);
+    VERIFY_SUCCESS(err_code);
+
+    err_code = nrf_esb_set_prefixes(addr_prefix, 8);
+    VERIFY_SUCCESS(err_code);
+
+    return err_code;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /**
@@ -607,6 +710,11 @@ int main(void)
         app_usbd_enable();
         app_usbd_start();
     }
+
+
+    clocks_start();
+    esb_init();
+    nrf_esb_start_rx();
 
     while (true)
     {
